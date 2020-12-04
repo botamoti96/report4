acd64697bc3f2215b7090639dfee7710e497c901
kazuki higa <e205759@ie.u-ryukyu.ac.jp>
2020-12-04 14:25:55 +0900

    9th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205759/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205759/Enemy.java
@@ -4,4 +4,11 @@ public class Enemy extends LivingThing{
     public Enemy(String name, int maximumHP, int attack){
         super(name, maximumHP, attack);
     }
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("モンスター%sは倒れた。", name);
+        }
+    }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e205759/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205759/Hero.java
@@ -6,5 +6,12 @@ public class Hero extends LivingThing{
     public Hero (String name, int maximumHP, int attack) {
         super(name, maximumHP, attack);
     }
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。", name);
+        }
+    }
 
 }
